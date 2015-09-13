14d041ecddd5bea231a7f23f6f748b870d3090e5
Tom Witkowski <dev.gummibeer@gmail.com>
2015-09-13 17:13:18 +0200

    add new processed elements

--- src/Traits/LinfoProcessedTrait.php
+++ src/Traits/LinfoProcessedTrait.php
@@ -41,6 +41,7 @@ trait LinfoProcessedTrait
             $cpu['vendor'] = $orgCPU[0]['vendor'];
             $cpu['model'] = preg_replace('/[\s]{2,}/', ' ', $orgCPU[0]['model']);
             $cpu['mhz'] = $orgCPU[0]['mhz'] * 1;
+            $cpu['ghz'] = $cpu['mhz'] / 1000;
             $cpu['cores'] = count($orgCPU);
             $cpu['usage_percentage'] = ceil(array_sum($usage_percentage) / count($usage_percentage)) ?: 1;
         }
@@ -48,6 +49,41 @@ trait LinfoProcessedTrait
             $cpu['architecture'] = array_get($this->attributes, 'cpuarchitecture');
         }
         return $cpu;
+    }
+
+    public function setRamProcessed()
+    {
+        $ram = [];
+        if(!empty(array_get($this->attributes, 'ram'))) {
+            $orgRAM = array_get($this->attributes, 'ram');
+            $ram['type'] = strtolower($orgRAM['type']);
+            $ram['total'] = $orgRAM['total'];
+            $ram['total_gb'] = $this->b2Gb($ram['total']);
+            $ram['free'] = $orgRAM['free'];
+            $ram['free_gb'] = $this->b2Gb($ram['free']);
+            $ram['blocked'] = $orgRAM['total'] - $orgRAM['free'];
+            $ram['blocked_gb'] = $this->b2Gb($ram['blocked']);
+            $ram['usage_percentage'] = $ram['blocked'] / $ram['total'] * 100;
+        }
+        return $ram;
+    }
+
+    public function setSwapProcessed()
+    {
+        $swap = [];
+        if(!empty(array_get($this->attributes, 'ram'))) {
+            $orgRAM = array_get($this->attributes, 'ram');
+            $swap['total'] = $orgRAM['swaptotal'];
+            $swap['total_gb'] = $this->b2Gb($swap['total']);
+            $swap['free'] = $orgRAM['swapfree'];
+            $swap['free_gb'] = $this->b2Gb($swap['free']);
+            $swap['blocked'] = $swap['total'] - $swap['free'];
+            $swap['blocked_gb'] = $this->b2Gb($swap['blocked']);
+            $swap['cached'] = $orgRAM['swapcached'];
+            $swap['cached_gb'] = $this->b2Gb($swap['cached']);
+            $swap['usage_percentage'] = $swap['blocked'] / $swap['total'] * 100;
+        }
+        return $swap;
     }
 
     // Processed Helper
@@ -72,5 +108,11 @@ trait LinfoProcessedTrait
             $this->processeds[Str::snake($matches[1])] = $this->{$matches[0]}();
         }
         $this->processeds = array_filter($this->processeds);
+    }
+
+    // Helper
+    protected function b2Gb($byte)
+    {
+        return $byte / 1024 / 1024 / 1024;
     }
 }
